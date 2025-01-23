2a857256ec1c151da5c5e5f65c97162aae1ee3aa
josineide silva <josineidesilva81305@gmail.com>
2025-01-23 13:54:42 -0300

    ajeitando caminho da imagem

--- js/script.js
+++ js/script.js
@@ -6,9 +6,9 @@ const gameOver = document.querySelector(".game-over");
 
 const restartButton = document.querySelector(".restart");
 
-const audio = new Audio("../audio/Canetaazul.mp3");
-const ai = new Audio("../audio/ai.mp3");
-const jumpSound = new Audio("../audio/jump.mp3");
+const audio = new Audio("./audio/Canetaazul.mp3");
+const ai = new Audio("./audio/ai.mp3");
+const jumpSound = new Audio("./audio/jump.mp3");
 
 audio.volume = 0.2;
 ai.volume = 0.5;
@@ -37,7 +37,7 @@ const start = () => {
 
   mario.style.bottom = "";
 
-  mario.src = "../img/manoel.gif";
+  mario.src = "./img/manoel.gif";
   // Substitua pelo caminho correto da imagem do Mario padrão
   cloud.style.left = "";
   cloud.style.animation = "cloud 20s infinite linear";
@@ -58,7 +58,7 @@ const start = () => {
       mario.style.animation = "none";
       mario.style.bottom = `${marioPosition}px`;
 
-      mario.src = "../img/game-over.png";
+      mario.src = "./img/game-over.png";
       ai.play();
       cloud.style.animation = "cloud 20s infinite linear";
       cloud.style.left = `${cloudPosition}px`;
